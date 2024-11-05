42a564e68b9b50386626a74bc3c429d929870128
Ubiquity <103650913+ufist@users.noreply.github.com>
2024-11-05 15:13:05 +0300

    Update fuzzbuff.py
    - Added Interactive Mode [-i]
    - Added a Receive Timeout
    - Shortened Banner Timeout

--- fuzzbuff.py
+++ fuzzbuff.py
@@ -17,9 +17,9 @@ def getH():
 	port = input("() Port: ")
 	return host,port
 
-def receive(sock):
+def receive(sock, timeout):
 	sock.setblocking(0)
-	ready = select.select([sock], [], [], 10)
+	ready = select.select([sock], [], [], timeout)
     
 	if ready[0]:
 		try:
@@ -28,7 +28,7 @@ def receive(sock):
 		except socket.error as e:
 			print(error,f"{e}")
 	else:
-		print(info, "Nothing received, continuing anyway")
+		print(info, f"Receive Timeout ({timeout})")
 
 def spattern(host, port, pattern, prefix, nw):
 	try:
@@ -37,8 +37,8 @@ def spattern(host, port, pattern, prefix, nw):
 		print(info,"Sending pattern...")
 		s.send(prefix.encode() + pattern.encode() + end.encode())
 		if not nw:
-			print(info,"Attempting to receive any banners (10s)...")
-			receive(s)
+			print(info,"Attempting to receive any banners (3s)...")
+			receive(s, 3)
 		print(good,"Pattern sent!")
 		s.close()
 		sys.exit()
@@ -64,8 +64,8 @@ def fuzzF(host, port, startf, endf, delay, char, pattern, prefix, nw):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect((host, port))
 		if not nw:
-			print(info,"Attempting to receive any banners (10s)...")
-			receive(s)
+			print(info,"Attempting to receive any banners (3s)...")
+			receive(s, 3)
 		fuzz = startf
 		while fuzz < endf:
 			print(good,fuzz)
@@ -74,13 +74,33 @@ def fuzzF(host, port, startf, endf, delay, char, pattern, prefix, nw):
 			payload += end.encode()
 			s.send(payload)
 			if not nw:
-				receive(s)
+				receive(s, timeout)
 			fuzz += startf
 			time.sleep(delay)
 		s.close()
 	except Exception as e:
 		print(error,f"{e}")
 
+def interactive(host, port, nw, timeout):
+	print("Entering Interactive mode... Use :e to exit")
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((host, port))
+		if not nw:
+			print(info,"Attempting to receive any banners (3s)...")
+			receive(s, 3)
+		while True:
+			payload = input(f"{host}:{port}> ")
+			if payload == ":e":
+				s.close()
+				sys.exit(1)
+			else:
+				pass
+			s.send(payload.encode())
+			if not nw:
+				receive(s, timeout)
+	except Exception as e:
+		print(error, f"{e}")
 def main():
 	parser = argparse.ArgumentParser(description="Fuzzing script with socket connection.")
 	parser.add_argument("host", help="Target host IP address")
@@ -92,6 +112,8 @@ def main():
 	parser.add_argument("-p", "--pattern", type=int, help="Send MSF Pattern, specify length")
 	parser.add_argument("-x", "--prefix", default="", help="Prefix before fuzz payload (MAKE SURE TO ADD SPACE IF NEEDED) [Ex: -x 'USER ']")
 	parser.add_argument("-n", "--no-receive", action="store_true", help="Do not attempt to receive any response from the host")
+	parser.add_argument("-i", "--interactive", action="store_true", help="Interactive mode")
+	parser.add_argument("-t", "--timeout", type=int, default=5, help="Receive Timeout")
 
 	args = parser.parse_args()
 
@@ -102,10 +124,16 @@ def main():
 	pattern = args.pattern
 	prefix = args.prefix
 	nw = args.no_receive
+	timeout = args.timeout
+	interactive_mode = args.interactive
+	
 	try:
 		host = args.host
 		port = args.port
-		fuzzF(host, port, startf, endf, delay, char, pattern, prefix, nw)
+		if not interactive_mode:
+			fuzzF(host, port, startf, endf, delay, char, pattern, prefix, nw, timeout)
+		else:
+			interactive(host, port, nw, timeout)
 	except Exception as e:
 		print(error, f"{e}")
 
